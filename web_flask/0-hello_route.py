435751c4fdf80d5ac39ea7e2d643fa5f7997864a
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2024-03-20 20:25:14 +0200

    starts flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-"""flask project"""
+
+"""
+This script defines a simple Flask web application.
+
+The application listens on 0.0.0.0, port 5000, and includes two routes:
+- '/' : This route displays "Hello HBNB!" when accessed.
+- '/hbnb' : This route displays "HBNB" when accessed.
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def main():
-    return "Hello HBNB!"
+def hello():
+    """
+    Display "Hello HBNB!" when accessing the root URL.
 
+    Returns:
+        str: A greeting message.
+    """
+    return 'Hello HBNB!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Display "HBNB" when accessing the '/hbnb' route.
+
+    Returns:
+        str: A message indicating the route.
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
